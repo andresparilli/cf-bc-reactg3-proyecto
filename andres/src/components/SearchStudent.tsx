52a479b9dd50684f36917e3d417cc45b7c50d6ae
Andres E. Parilli <andres.parilli@gmail.com>
2024-03-28 16:29:40 -0400

    Listado de esudiantes y estructura de buscador

--- andres/src/components/SearchStudent.tsx
+++ andres/src/components/SearchStudent.tsx
@@ -1,18 +1,23 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 
 const SearchStudent = () => {
+    const [search, setSearch] = useState('');
+    
+    const  handleInputChange = (evt) => {
+        setSearch(evt.target.value);
+    }
+    const handleSubmit = () => {
+        alert(search);
+    }
+
     return (
 
-        <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Buscar"
-              className="me-4"
-              aria-label="Buscar"
-            />
-            <Button variant="outline-success bg-dark">Buscar</Button>
+        <Form className="d-flex" onSubmit={handleSubmit}>
+            <Form.Control type="search" placeholder="Buscar" className="me-4" aria-label="Buscar"  onChange={handleInputChange} value={search}/>
+            <Button type="submit" variant="outline-success bg-dark">Buscar</Button>
         </Form>
     );
 };
