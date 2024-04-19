41b02698099e3d7b7aa1c86ba52f7cc5e576bbab
Andres E. Parilli <andres.parilli@gmail.com>
2024-04-19 00:30:49 -0400

    se agrega logo

--- andres/src/views/home.tsx
+++ andres/src/views/home.tsx
@@ -1,12 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
+import Logo from '../assets/THE_U_FOR_YOU_LOGO_TRANSPARENCIA_9.png';
+import styles from './home.module.css';
 
 const Home = () => {
     return (
 
-        <Container className="p-3">
+        <Container className="p-6">
       <Card>
-        <Card.Img variant="top" src="../assets/react.svg" />
+        <Card.Img src={Logo} className={styles.logo} />
         <Card.Body>
           <Card.Text>
             "Somos tu one stop de estudios en el exterior."
